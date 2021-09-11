6ba21b2dc2d5dd4e67e3a09b7f038e8b2162e41a
Gamotronic <79149622+GamotronicYT@users.noreply.github.com>
2021-09-11 15:44:28 +0200

    Add files via upload

---
+++ FortniteUniversalLauncher/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace FortniteUniversalLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FortnitePatch (Fortnite) => ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dll 1 => ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dll 2 (optionnal) => ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(638, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 178);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(724, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(116, 238);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(672, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(692, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 55);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Launch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Made by Gamotronic#1025";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(150, 423);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Join Discord";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(367, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "FortnitePatch (s1) [don\'t fill the label if you not want to launch s1] {Win64}=> " +
+    "";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(375, 135);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(413, 20);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Fortnite Universal Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ FortniteUniversalLauncher/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
+using FortniteUniversalLauncher.injector;
+using System.Collections;
+using System.Threading;
+using System.Text.RegularExpressions;
+using FortniteUniversalLauncher.Properties;
+namespace FortniteUniversalLauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = Settings.Default.fn;
+            textBox2.Text = Settings.Default.dll1;
+            textBox3.Text = Settings.Default.dll2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                if (textBox2.Text == null)
+                {
+                    MessageBox.Show("You Need to give a Dll1 patch");
+                }
+                else if (textBox2.Text == "")
+                {
+                    MessageBox.Show("You Need to give a Dll1 patch");
+                }
+                else
+                {
+                    if (textBox1.Text == null)
+                    {
+                        Settings.Default["fn"] = textBox1.Text;
+                        Settings.Default.Save();
+                        Settings.Default["fns1"] = textBox4.Text;
+                        Settings.Default.Save();
+                        Settings.Default["dll1"] = textBox2.Text;
+                        Settings.Default.Save();
+                        Settings.Default["dll2"] = textBox3.Text;
+                        Settings.Default.Save();
+                        Process process3 = StartProcess(textBox4.Text + "/FortniteClient-Win64-Shipping.exe", false, "");
+                        process3.WaitForInputIdle();
+                        base.Hide();
+                        if (textBox2.Text == null)
+                        {
+                            inject.InjectDll(process3.Id, textBox2.Text);
+                            MessageBox.Show("Injected Dll1");
+                        }
+                        else if (textBox2.Text == "")
+                        {
+                            inject.InjectDll(process3.Id, textBox2.Text);
+                            MessageBox.Show("Injected Dll1");
+                        }
+                        else
+                        {
+                            inject.InjectDll(process3.Id, textBox2.Text);
+                            inject.InjectDll(process3.Id, textBox3.Text);
+                            MessageBox.Show("Injected Dll1 and Dll2");
+                        }
+
+                        process3.WaitForExit();
+                        base.Show();
+                    }
+                    else if (textBox1.Text == "")
+                    {
+                        Settings.Default["fn"] = textBox1.Text;
+                        Settings.Default.Save();
+                        Settings.Default["fns1"] = textBox4.Text;
+                        Settings.Default.Save();
+                        Settings.Default["dll1"] = textBox2.Text;
+                        Settings.Default.Save();
+                        Settings.Default["dll2"] = textBox3.Text;
+                        Settings.Default.Save();
+                        Process process3 = StartProcess(textBox4.Text + "/FortniteClient-Win64-Shipping.exe", false, "");
+                        process3.WaitForInputIdle();
+                        base.Hide();
+                        if (textBox2.Text == null)
+                        {
+                            inject.InjectDll(process3.Id, textBox2.Text);
+                            MessageBox.Show("Injected Dll1");
+                        }
+                        else if (textBox2.Text == "")
+                        {
+                            inject.InjectDll(process3.Id, textBox2.Text);
+                            MessageBox.Show("Injected Dll1");
+                        }
+                        else
+                        {
+                            inject.InjectDll(process3.Id, textBox2.Text);
+                            inject.InjectDll(process3.Id, textBox3.Text);
+                            MessageBox.Show("Injected Dll1 and Dll2");
+                        }
+
+                        process3.WaitForExit();
+                        base.Show();
+                    }
+                    else
+                    {
+                        Settings.Default["fn"] = textBox1.Text;
+                        Settings.Default.Save();
+                        Settings.Default["fns1"] = textBox1.Text;
+                        Settings.Default.Save();
+                        Settings.Default["dll1"] = textBox2.Text;
+                        Settings.Default.Save();
+                        Settings.Default["dll2"] = textBox3.Text;
+                        Settings.Default.Save();
+                        Process process = StartProcess(textBox1.Text + "/FortniteGame/Binaries/Win64/FortniteLauncher.exe", true, "-NOSSLPINNING");
+                        Process process2 = StartProcess(textBox1.Text + "/FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping_BE.exe", true, "");
+                        Process process3 = StartProcess(textBox1.Text + "/FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe", false, "");
+                        process3.WaitForInputIdle();
+                        base.Hide();
+                        if (textBox2.Text == null)
+                        {
+                            inject.InjectDll(process3.Id, textBox2.Text);
+                            MessageBox.Show("Injected Dll1");
+                        }
+                        else if (textBox2.Text == "")
+                        {
+                            inject.InjectDll(process3.Id, textBox2.Text);
+                            MessageBox.Show("Injected Dll1");
+                        }
+                        else
+                        {
+                            inject.InjectDll(process3.Id, textBox2.Text);
+                            inject.InjectDll(process3.Id, textBox3.Text);
+                            MessageBox.Show("Injected Dll1 and Dll2");
+                        }
+
+                        process3.WaitForExit();
+                        process.Kill();
+                        process2.Kill();
+                        base.Show();
+                    }
+                }
+        }
+        public static Process StartProcess(string path, bool shouldFreeze, string extraArgs = "")
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = path,
+                    Arguments = $"-epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -noeac -fromfl=be -fltoken=5dh74c635862g575778132fb -skippatchcheck" + extraArgs
+                }
+            };
+            process.Start();
+            if (shouldFreeze)
+            {
+                foreach (object obj in process.Threads)
+                {
+                    ProcessThread thread = (ProcessThread)obj;
+                    Win32.SuspendThread(Win32.OpenThread(2, false, thread.Id));
+                }
+            }
+            return process;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start("MicrosoftEdge.exe", "https://discord.gg/kzDTDsSmEs");
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
